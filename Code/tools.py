c1e353993315ba1e1132a139077fca8bff3797fd
alanloret <alan.loret@ensae.fr>
2021-04-20 13:07:14 +0200

    Add uniform on [-pi, pi].

--- Code/tools.py
+++ Code/tools.py
@@ -1,1 +1,8 @@
 import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+
+def uniform_pi(n):
+    """ This function simulates a random variable on [-pi, pi]. """
+    return np.random.uniform(-1, 1, n) * np.pi
