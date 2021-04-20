c007967c62d3883592dcd889fd16a52ca14d6933
alanloret <alan.loret@ensae.fr>
2021-04-21 00:19:58 +0200

    Corrections and improvements

--- Code/__main__.py
+++ Code/__main__.py
@@ -4,7 +4,7 @@ from scipy.stats import vonmises
 
 
 class VonMises:
-    def __init__(self, mu=0, kappa=3):
+    def __init__(self, mu=0., kappa=3.):
         """
         Args:
             mu: Paramètre mu de la loi de Von Mises.
@@ -13,9 +13,9 @@ class VonMises:
         self.mu = mu
         self.kappa = kappa
 
-    def generate(self, n=1):
+    def simulate(self, n=1):
         """
-Génère n observation(s) sous la loi de Von Mises.
+        Génère n observation(s) sous la loi de Von Mises.
         Args:
             n: nombre d'observations souhaitées.
 
@@ -23,30 +23,38 @@ Génère n observation(s) sous la loi de Von Mises.
         """
         return von_mises_unif(self.mu, self.kappa, n)
 
-    def generate_graph(self, n=1000):
+    def hist(self, n=1000):
         """
-Génère le graphe de n observations sous la loi de Von Mises.
+        Génère le graphe de n observations sous la loi de Von Mises.
         Args:
             n: nombre d'observations souhaitées.
         """
         data = von_mises_unif(self.mu, self.kappa, n)
-        fig, ax = plt.subplots(1, 1)
-        ax.hist(data, bins=500, density=True)
-        x = np.linspace(vonmises.ppf(0.01, self.kappa, loc=self.mu), vonmises.ppf(0.99, self.kappa, loc=self.mu), 100)
+        plt.gcf().clear()
+        fig = plt.figure(figsize=(10, 6))
+        ax = fig.add_axes([0.065, 0.1, 0.75, 0.8])
+        
+        ax.hist(data, bins=500, density=True, color='grey')
+        x = np.linspace(-np.pi, np.pi, 100)
         ax.plot(x, vonmises.pdf(x, self.kappa, loc=self.mu), 'r-', lw=1, label='theoretical')
+
+        ax.text(3.85, 0.15,
+                f'mu:         {self.mu:.3f}\nkappa:    {self.kappa:.3f}\nn:       {n:.1e}',
+                style='italic',
+                bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
         ax.set_title('Von Mises simulation')
         plt.legend()
         plt.show()
 
 
-#Note pour le groupe : il faut peut-être penser à des moyens d'évaluer notre modèle ?
+# Note pour le groupe : il faut peut-être penser à des moyens d'évaluer notre modèle ?
 
 if __name__ == '__main__':
-    mu = 0
-    kappa = 3
-    n = 100_000
-    model = VonMises(mu, kappa)
-    result = model.generate()  # Génère un résultat de notre modèle Von Mises.
-    print(result)
-    model.generate_graph(n)  # Génère un graphe à n observations.
+    mu = np.pi/4
+    kappa = 0.7
+    n = 1_000_000
+    model = VonMises(mu=mu, kappa=kappa)
+    result = model.simulate(n=n)  # Génère un résultat de notre modèle Von Mises.
+
+    model.hist(n=n)  # Génère un graphe à n observations.
 
