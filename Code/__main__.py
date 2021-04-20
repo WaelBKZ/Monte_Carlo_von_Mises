1d7756530197d88543b0d6fad8a01bb27be1c398
alanloret <alan.loret@ensae.fr>
2021-04-20 14:54:53 +0200

    First simulation

--- Code/__main__.py
+++ Code/__main__.py
@@ -1,4 +1,21 @@
 from tools import *
+import matplotlib.pyplot as plt
+from scipy.stats import vonmises
 
 if __name__ == '__main__':
-    pass
+    mu = 0
+    kappa = 3
+    n = 100_000
+
+    data = von_mises_unif(mu=mu, kappa=kappa, n=n)
+    
+    fig, ax = plt.subplots(1, 1)
+    ax.hist(data, bins=500, density=True)
+
+    x = np.linspace(vonmises.ppf(0.01, kappa, loc=mu), vonmises.ppf(0.99, kappa, loc=mu), 100)
+    ax.plot(x, vonmises.pdf(x, kappa, loc=mu), 'r-', lw=1, label='theoretical')
+    ax.set_title('Von Mises simulation')
+    plt.legend()
+    plt.show()
+
+
