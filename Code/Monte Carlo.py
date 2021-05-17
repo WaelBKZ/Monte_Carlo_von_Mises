c71110b6ebaa6ace7a0d1f1b1c3682582c3469e8
Waël <wael.boukhobza@ensae.fr>
2021-05-18 00:47:17 +0200

    added a fit method for RWHM

--- Code/Monte Carlo.py
+++ Code/Monte Carlo.py
@@ -64,15 +64,17 @@ class ProjectModel:
         mu_, kappa_ = params_estimated.mean(axis=0)
         mu_std, kappa_std = params_estimated.std(axis=0) * np.sqrt(n / (n - 1))
 
-        print(f"Parameters:\n\u03BC = {mu_:.5f}  [{mu_ - 1.96 * mu_std / n**0.5:.5f}, {mu_ + 1.96 * mu_std / n**0.5:.5f}]")
-        print(f"\u03BA = {kappa_:.5f}  [{kappa_ - 1.96 * kappa_std / n**0.5:.5f}, {kappa_ + 1.96 * kappa_std / n**0.5:.5f}]\n")
+        print(
+            f"Parameters:\n\u03BC = {mu_:.5f}  [{mu_ - 1.96 * mu_std / n ** 0.5:.5f}, {mu_ + 1.96 * mu_std / n ** 0.5:.5f}]")
+        print(
+            f"\u03BA = {kappa_:.5f}  [{kappa_ - 1.96 * kappa_std / n ** 0.5:.5f}, {kappa_ + 1.96 * kappa_std / n ** 0.5:.5f}]\n")
 
         return np.array([[mu_, mu_std], [kappa_, kappa_std]])
 
     def describe_mu(self, save=False):
         """ This function plots the density of a von Mises distribution for different values of mu """
 
-        mu = [-np.pi/2, 0, np.pi/2]
+        mu = [-np.pi / 2, 0, np.pi / 2]
         x = np.linspace(-np.pi, np.pi, 300)
 
         fig, ax = plt.subplots(1, 3, figsize=(12, 5), sharey=True)
@@ -81,7 +83,7 @@ class ProjectModel:
         for val in mu:
             ax[i].plot(x, vonmises.pdf(x, self.kappa, loc=val), 'r-', lw=1)
             ax[i].set_ylim([0., None])
-            ax[i].set_xticks([-3.14, 0.,  3.14])
+            ax[i].set_xticks([-3.14, 0., 3.14])
             i += 1
 
         ax[0].title.set_text(f'\u03BC = -\u03C0/2')
@@ -114,11 +116,11 @@ class ProjectModel:
 
         for val in kappa:
             if val != 0:
-                ax[i//3, i % 3].plot(x, vonmises.pdf(x, val, loc=0.), 'r-', lw=1)
-                ax[i//3, i % 3].set_ylim([0., None])
-                ax[i//3, i % 3].set_xticks([-3.14, 0.,  3.14])
-                ax[i//3, i % 3].locator_params(axis="y", nbins=4)
-                ax[i//3, i % 3].title.set_text(f'\u03BA = {val}')
+                ax[i // 3, i % 3].plot(x, vonmises.pdf(x, val, loc=0.), 'r-', lw=1)
+                ax[i // 3, i % 3].set_ylim([0., None])
+                ax[i // 3, i % 3].set_xticks([-3.14, 0., 3.14])
+                ax[i // 3, i % 3].locator_params(axis="y", nbins=4)
+                ax[i // 3, i % 3].title.set_text(f'\u03BA = {val}')
                 i += 1
 
         fig.suptitle('von Mises (\u03BC = 0)')
@@ -198,17 +200,19 @@ class VonMisesAcceptReject(ProjectModel):
             ax[i].plot(x, vonmises.pdf(x, self.kappa, loc=self.mu), 'r-', lw=1, label='theoretical')
 
             if self.proposal == 'uniform':
-                ax[i].plot([-np.pi, np.pi], 2 * [vonmises.pdf(x, self.kappa, loc=self.mu).max()], 'b-', lw=1, label='proposal')
+                ax[i].plot([-np.pi, np.pi], 2 * [vonmises.pdf(x, self.kappa, loc=self.mu).max()], 'b-', lw=1,
+                           label='proposal')
 
             elif self.proposal == 'cauchy':
                 y, x = np.histogram(wrapped_cauchy(mu=self.mu, kappa=self.kappa, n=10_000_000),
                                     bins=np.linspace(-np.pi, np.pi, 1_000), density=True)
                 y_, _ = np.histogram(von_mises_cauchy(mu=self.mu, kappa=self.kappa, n=10_000_000),
                                      bins=np.linspace(-np.pi, np.pi, 1_000), density=True)
-                ax[i].plot((x[:-1] + x[1:]) / 2, (y_ / y).max() * gaussian_filter1d(y, sigma=10), 'b-', lw=1, label='proposal')
+                ax[i].plot((x[:-1] + x[1:]) / 2, (y_ / y).max() * gaussian_filter1d(y, sigma=10), 'b-', lw=1,
+                           label='proposal')
 
             ax[i].set_ylim([0., None])
-            ax[i].set_xticks([-3.14, 0.,  3.14])
+            ax[i].set_xticks([-3.14, 0., 3.14])
             ax[i].title.set_text(f'n = {val:.1e}')
             i += 1
 
@@ -230,9 +234,10 @@ class VonMisesAcceptReject(ProjectModel):
         kappa = np.linspace(0, 50, 300)
 
         fig, ax = plt.subplots(figsize=(10, 5))
-        ax.plot(kappa, [von_mises_cauchy_acceptance(kappa=val, n=n) for val in kappa], 'b-', lw=1, label='wrapped cauchy')
+        ax.plot(kappa, [von_mises_cauchy_acceptance(kappa=val, n=n) for val in kappa], 'b-', lw=1,
+                label='wrapped cauchy')
         ax.plot(kappa, [von_mises_unif_acceptance(kappa=val, n=n) for val in kappa], 'r-', lw=1, label='uniform')
-        ax.set_yticks([0,  25, 50, 75, 100])
+        ax.set_yticks([0, 25, 50, 75, 100])
 
         ax.grid(True, linewidth=0.5, color='grey', linestyle='-')
         ax.set_xlabel("\u03BA")
@@ -282,7 +287,7 @@ class VonMisesRWHM(ProjectModel):
         else:
             return x
 
-    def simulate(self, n=100_000):
+    def simulate(self, n=100_000, print_acceptance_rates=True):
         """
         Generates n simulations under our von Mises Random Walk Hastings-Metropolis simulation.
         :param int n: number of simulations
@@ -298,7 +303,8 @@ class VonMisesRWHM(ProjectModel):
             x[i] = self.iter(x[i - 1])
 
         self.acceptance_rate = self.n_accept / n
-        print(f'Acceptance rate: {self.acceptance_rate} %.')  # should be calibrated between 25% and 40%;
+        if print_acceptance_rates == True: print(
+            f'Acceptance rate: {self.acceptance_rate * 100} %.')  # should be calibrated between 25% and 40%;
 
         self.results = x
         return self.results
@@ -318,7 +324,7 @@ class VonMisesRWHM(ProjectModel):
             ax[i].plot(x, vonmises.pdf(x, self.kappa, loc=self.mu), 'r-', lw=1, label='theoretical')
 
             ax[i].set_ylim([0., None])
-            ax[i].set_xticks([-3.14, 0.,  3.14])
+            ax[i].set_xticks([-3.14, 0., 3.14])
             ax[i].title.set_text(f'n = {val:.1e}')
             i += 1
 
@@ -326,11 +332,34 @@ class VonMisesRWHM(ProjectModel):
         ax[1].get_yaxis().set_visible(False)
         ax[2].get_yaxis().set_visible(False)
 
-        fig.suptitle(f'von Mises  (\u03BC = {self.mu:.3f},  \u03BA = {self.kappa:.3f},  proposal_RWHM = {self.proposal_RWHM})')
+        fig.suptitle(
+            f'von Mises  (\u03BC = {self.mu:.3f},  \u03BA = {self.kappa:.3f},  proposal_RWHM = {self.proposal_RWHM})')
         plt.legend(prop={'size': 8})
         plt.show()
         if save:
             fig.savefig('Graphs/describe_simulation_RWHM_' + self.proposal_RWHM + '.png')
+
+    def fit(self, len_batch=1000, num_iter=1000):
+        """
+        Chooses an optimal value for sigma, the standard deviation of our random walk. Keep in mind when choosing
+        parameters that the number of simulation that will be computed is len_batch * num_iter.
+        :param int len_batch: number of simulations for each Von Mises estimation.
+        :param int num_iter: number of sigma that we want to try.
+        :return NoneType: None. Prints the advised value for sigma.
+        """
+        old_sig = self.sig  # Stocks the value of sigma initially entered by user.
+        step = 10 / num_iter
+        sigma_proposals = [step * i for i in range(1, num_iter)]
+        for i in range(num_iter):
+            sig = sigma_proposals[i]
+            self.sig = sig
+            self.simulate(len_batch, print_acceptance_rates=False)
+            if 0.3 < self.acceptance_rate < 0.4:
+                print(f'Recommended value for sigma : {sig}. Acceptance rate around {self.acceptance_rate}.')
+                print(i)
+                return None
+        self.sig = old_sig  # In order not to modify the sigma value wished by the user.
+        print("Couldn't find a sigma that satisfies the acceptance rate criteria.")
 
     def graph_chain(self, n_points=1000):
         """
@@ -356,23 +385,22 @@ class VonMisesRWHM(ProjectModel):
 
 if __name__ == '__main__':
     # Parameters common to every model:
-    mu = 0.
-    kappa = 1.
-    n = 1_000_000
+    mu = 2
+    kappa = 14
+    n = 1_0000
     proposal = 'cauchy'
     save = False
 
     """ DESCRIBE """
-    model = VonMisesAcceptReject(mu=mu, kappa=kappa)
-    # model.describe_mu(save=save)  # plots the density for different values of mu
-    # model.describe_kappa(save=save)  # plots the density for different values of kappa
-    # model.describe_simulation(save=save)  # plots the simulation for different values of n
-    # VonMisesAcceptReject(mu=mu, kappa=kappa, proposal='uniform').describe_simulation(save=save)  # plots the simulation for different values of n
-    # model.estimate_params_MCMC_MLE()  # estimates the parameters of the model by MCMC MLE
+    # model = VonMisesAcceptReject(mu=mu, kappa=kappa) model.describe_mu(save=save)  # plots the density for
+    # different values of mu model.describe_kappa(save=save)  # plots the density for different values of kappa
+    # model.describe_simulation(save=save)  # plots the simulation for different values of n VonMisesAcceptReject(
+    # mu=mu, kappa=kappa, proposal='uniform').describe_simulation(save=save)  # plots the simulation for different
+    # values of n model.estimate_params_MCMC_MLE()  # estimates the parameters of the model by MCMC MLE
     # model.acceptance_rate_simulation(save=save)  # plots the acceptance rate against kappa
 
     """ SIMULATE """
-    model = VonMisesAcceptReject(mu=mu, kappa=kappa, proposal=proposal)
+    # model = VonMisesAcceptReject(mu=mu, kappa=kappa, proposal=proposal)
     # model.simulate(n=n)  # generates n observations under the Accept-Reject simulation
     # model.hist()  # generates the histogram of the above observations
 
@@ -382,7 +410,9 @@ if __name__ == '__main__':
     proposal_RWHM = 'gaussian'  # 'gaussian' or 'uniform'
     sig = 5.5
 
-    model_RWHM = VonMisesRWHM(mu=mu, kappa=kappa, x_init=x_init, proposal=proposal, proposal_RWHM=proposal_RWHM, sig=sig)
+    model_RWHM = VonMisesRWHM(mu=mu, kappa=kappa, x_init=x_init, proposal=proposal, proposal_RWHM=proposal_RWHM,
+                              sig=sig)
+    model_RWHM.fit(len_batch=100, num_iter=1_000)
     # model_RWHM.simulate(n=n)  # generates n observations under the Random Walk Hastings-Metropolis simulation;
     # model_RWHM.hist()  # generates the histogram of the above observations;
     # model_RWHM.describe_simulation(save=save)  # plots the simulation for different values of n
